2fae0caaa256a80a80855740e02a1550fcea155f
Gourav Sharma <gourav.sharma.glamat2932@gmail.com>
2025-01-16 21:20:18 +0530

    Update main.py

--- Plant_Disease_Prediction/main.py
+++ Plant_Disease_Prediction/main.py
@@ -5,10 +5,9 @@ import numpy as np
 #Tensorflow Model Prediction
 def model_prediction(test_image):
     model  = tf.keras.models.load_model('Plant_Disease_Prediction/trained_model.keras')
-    image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
-    input_arr = np.array([input_arr])  # Convert single image to a batch
-    input_arr = input_arr / 255.0  # Normalize image
+    input_arr = np.array([input_arr]) #Convert single image to a batch
     prediction = model.predict(input_arr)
     result_index = np.argmax(prediction)
     return result_index
